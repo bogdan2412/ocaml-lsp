59ebb356e5a526c77c285fc300bb45227846d271
Bogdan-Cristian Tătăroiu <b.tataroiu@gmail.com>
2025-12-29 16:33:58 +0000

    Fix build on FreeBSD.

--- submodules/lev/lev/vendor/ev.c
+++ submodules/lev/lev/vendor/ev.c
@@ -507,7 +507,12 @@
 #endif
 
 #if EV_USE_INOTIFY
-# include <sys/statfs.h>
+# ifdef __FreeBSD__
+#  include <sys/param.h>
+#  include <sys/mount.h>
+# else
+#  include <sys/statfs.h>
+# endif
 # include <sys/inotify.h>
 /* some very old inotify.h headers don't have IN_DONT_FOLLOW */
 # ifndef IN_DONT_FOLLOW
